0b9b7f20c2a74b0a332e9e1f06210d0f09c176a7
drozdiam <drozdovskiy_ilya@mail.ru>
2023-03-05 19:09:08 +0300

    12

---
+++ app/Http/Controllers/MyPlaseController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyPlaseController extends Controller
+{
+    public function index(){
+        return 'this is my page';
+    }
+}
